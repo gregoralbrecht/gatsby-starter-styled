c9f3a1245407895afa9d6566bfd59e32ab347488
Gregor Albrecht <hello@gregoralbrecht.com>
2019-04-14 21:23:43 +0200

    Use traced svg instead of blur up effect

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -20,7 +20,7 @@ const Image = () => (
         placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
+              ...GatsbyImageSharpFluid_tracedSVG
             }
           }
         }
