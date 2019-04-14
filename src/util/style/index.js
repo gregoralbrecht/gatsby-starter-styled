9e95b0cf8537402572bce2cfa37a2bbf85905bb5
Gregor Albrecht <hello@gregoralbrecht.com>
2019-04-14 18:55:47 +0200

    Upgrade all dependencies
    Bump all package to their latest versions and fix a small change for
    styled-system.

--- src/util/style/index.js
+++ src/util/style/index.js
@@ -1,10 +1,10 @@
 import * as styledComponents from 'styled-components'
-import { themeGet, util } from 'styled-system'
+import { themeGet, px } from 'styled-system'
 
 import { theme } from './theme'
 
 // Utility function to get pixel values from a unitless scale in our custom theme
-const themePx = (...args) => props => util.px(themeGet(...args)(props))
+const themePx = (...args) => props => px(themeGet(...args)(props))
 
 const {
   default: styled,
